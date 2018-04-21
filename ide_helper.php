defea890bb6b5d9db7bc2ff24012b46b6106ee2f
Dmytro Kulyk <lnkvisitor.ts@gmail.com>
2018-04-21 10:46:45 +0300

    Update IDE helpers.

--- ide_helper.php
+++ ide_helper.php
@@ -7,23 +7,32 @@ namespace Illuminate\Database\Eloquent {
      * Class Builder
      *
      * @package Illuminate\Database\Eloquent
+     * @method static withDisabled()
+     * @method static withoutDisabled()
+     * @method static withTrashed()
+     * @method static withoutTrashed()
+     * @method static onlyTrashed()
+     * @method static restore()
      */
     class Builder
     {
-        /**
-         * @return Builder
-         */
-        public function withDisabled(): Builder
-        {
-            return $this;
-        }
+    }
+}
 
-        /**
-         * @return Builder
-         */
-        public function withoutDisabled(): Builder
-        {
-            return $this;
-        }
+namespace Illuminate\Support {
+
+    /**
+     * @method static nullable()
+     * @method static unique()
+     * @method static index()
+     * @method static default($value)
+     * @method static references(string $field)
+     * @method static on(string $table)
+     * @method static onUpdate(string $action)
+     * @method static onDelete(string $action)
+     * @method static after(string $field)
+     */
+    class Fluent
+    {
     }
 }
