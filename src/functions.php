4d4a46084a98a1617c1f3c36a2f903ec477d2d9e
Dmytro Kulyk <dmytro@dkulyk.name>
2020-07-09 12:46:15 +0300

    CS

--- src/functions.php
+++ src/functions.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 use DKulyk\Eloquent\Extensions\Support\PendingDispatch;
 
-if (!function_exists('dispatch_transaction')) {
+if (! function_exists('dispatch_transaction')) {
     function dispatch_transaction($job)
     {
         if ($job instanceof Closure) {
