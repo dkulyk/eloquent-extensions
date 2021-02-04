5b67b2c78b16853b6ed1367f16f704988891df67
Dmytro Kulyk <lnkvisitor.ts@gmail.com>
2021-02-04 09:21:30 +0200

    Fixes isDefault

--- src/Concerns/HasDefaults.php
+++ src/Concerns/HasDefaults.php
@@ -14,11 +14,6 @@ use Illuminate\Database\Eloquent\Model;
  */
 trait HasDefaults
 {
-    /**
-     * Boot the soft deleting trait for a model.
-     *
-     * @return void
-     */
     public static function bootHasDefaults(): void
     {
         static::saved(function (Model $model) {
@@ -35,42 +30,27 @@ trait HasDefaults
 
     protected function initializeHasDefaults(): void
     {
-        $this->fillable[] = $field = $this->getDefaultsColumn();
+        $field = $this->getDefaultsColumn();
+        $this->fillable[] = $field;
         $this->casts[$field] = 'bool';
     }
 
-    /**
-     * Get the name of the "deleted at" column.
-     *
-     * @return string
-     */
     public function getDefaultsColumn(): string
     {
         return 'default';
     }
 
-    /**
-     * Get the fully qualified "deleted at" column.
-     *
-     * @return string
-     */
     public function getQualifiedDefaultsColumn(): string
     {
         return "{$this->getTable()}.{$this->getDefaultsColumn()}";
     }
 
 
-    /**
-     * @return bool
-     */
     public function isDefault(): bool
     {
-        return (bool) $this->getAttribute($this->getDefaultAttribute());
+        return (bool) $this->getAttribute($this->getDefaultsColumn());
     }
 
-    /**
-     * @return Builder
-     */
     protected function unsetDefaultsQuery(): Builder
     {
         return $this->newQueryWithoutScopes();
